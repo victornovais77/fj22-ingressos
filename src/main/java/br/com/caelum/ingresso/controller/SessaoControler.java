8dbd151d51f99b99acc0e8cfda814053100ba60a
victornovais77 <victornovais77@gmail.com>
2018-06-30 14:51:10 -0300

    Salvando A Sessao No Sistema

--- src/main/java/br/com/caelum/ingresso/controller/SessaoControler.java
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoControler.java
@@ -1,13 +1,21 @@
 package br.com.caelum.ingresso.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.caelum.ingresso.dao.FilmeDao;
 import br.com.caelum.ingresso.dao.SalaDao;
+import br.com.caelum.ingresso.dao.SessaoDao;
+import br.com.caelum.ingresso.model.Sessao;
+import br.com.caelum.ingresso.model.form.SessaoForm;
 
 @Controller
 public class SessaoControler {
@@ -18,15 +26,35 @@ public class SessaoControler {
 	@Autowired
 	private SalaDao salaDao;
 	
+	@Autowired
+	private SessaoDao sessaoDao;
+	
 	@GetMapping("/admin/sessao")
-	public ModelAndView from(@RequestParam("salaId") int salaId){
+	public ModelAndView form(@RequestParam("salaId") int salaId,SessaoForm form){
 		
 		ModelAndView mv = new ModelAndView("sessao/sessao");
 		
 		mv.addObject("sala",salaDao.findOne(salaId));
 		mv.addObject("filme", filmeDao.findAll());
+		mv.addObject("form",form);
+		
 		
 		return mv;
+	}
+	
+	@PostMapping("/admin/sessao")
+	@Transactional
+	public ModelAndView salva(@Valid SessaoForm form,BindingResult result){
+		
+		if(result.hasErrors()){
+			
+			Sessao sessao = form.toSessao(salaDao, filmeDao);
+			
+			sessaoDao.save(sessao);
+					
+		}
+		
+		return new ModelAndView("redirect:/admin/sala/"+ form.getSalaId() +"sessoes");
 	}
     
 
--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -5,20 +5,29 @@ import java.time.LocalTime;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Sessao {
-	
+
 	@Id
 	@GeneratedValue
 	private Integer id;
+	
+	
 	private LocalTime horario;
+	
+	@ManyToOne
 	private Sala sala;
+	
+	@ManyToOne
 	private Filme filme;
 	
-	public Sessao() {}
-	
+
+	public Sessao() {
+
+	}
+
 	public Sessao(Integer id, LocalTime horario, Sala sala, Filme filme) {
 		super();
 		this.id = id;
@@ -26,35 +35,41 @@ public class Sessao {
 		this.sala = sala;
 		this.filme = filme;
 	}
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public LocalTime getHorario() {
 		return horario;
 	}
+
 	public void setHorario(LocalTime horario) {
 		this.horario = horario;
 	}
+
 	public Sala getSala() {
 		return sala;
 	}
+
 	public void setSala(Sala sala) {
 		this.sala = sala;
 	}
+
 	public Filme getFilme() {
 		return filme;
 	}
+
 	public void setFilme(Filme filme) {
 		this.filme = filme;
 	}
-	
-	public LocalTime getHorarioTermino(){
+
+	public LocalTime getHorarioTermino() {
 		return this.horario.plusMinutes(filme.getDuracao().toMinutes());
 	}
-	
-	
 
 }
